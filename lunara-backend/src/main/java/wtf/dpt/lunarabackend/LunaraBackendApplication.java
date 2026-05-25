0218cf47fc501dc7dd6abd22039157e9e65dd2b3
Michal Marusinec <michalmarusinec@gmail.com>
2026-05-25 18:27:27 +0200

    Initial commit: Add Lunara multimodule project structure with backend, web, and mobile modules

---
+++ lunara-backend/src/main/java/wtf/dpt/lunarabackend/LunaraBackendApplication.java
@@ -0,0 +1,13 @@
+package wtf.dpt.lunarabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunaraBackendApplication.class, args);
+    }
+
+}
